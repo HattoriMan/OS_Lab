f40f0e0417e0d8d538eb5fffad91c97ad2575511
HattoriMan <manavs19102006@gmail.com>
2026-01-22 15:56:16 +0530

    Add files from Stage 1 to Stage 10

---
+++ Stage_1/setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install libreadline-dev flex bison make gcc wget curl
+curl -sSf https://raw.githubusercontent.com/eXpOSNitc/expos-bootstrap/main/download.sh | sh
+cd $HOME/myexpos 
+make
